3f07d035dfb05d59b15ffcacf88fa54ef074ef70
NtsDK <ghoul71@yandex.ru>
2019-08-03 18:42:17 +0200

    Published build

---
+++ sensorClustering/precache-manifest.789f5d3669bd46762b277071b25ab093.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5512d0e0f314e6779e60f1fcb32c21f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a6eb024baf6c3e533efb",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "e91168ddfefe91d9f37b",
+    "url": "/static/css/main.ed37c868.chunk.css"
+  },
+  {
+    "revision": "a6eb024baf6c3e533efb",
+    "url": "/static/js/2.b84f0008.chunk.js"
+  },
+  {
+    "revision": "e91168ddfefe91d9f37b",
+    "url": "/static/js/main.960747de.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
