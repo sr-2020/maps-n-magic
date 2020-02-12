b56466245e02c0350744717b10cbfb5ab7a5aac4
NtsDK <trechkalov@yandex.ru>
2020-02-12 01:14:53 +0100

    Added showing of all available data in user scatter chart.

--- src/components/RealTrackStats/ScatterRow/prepareScatterData.js
+++ src/components/RealTrackStats/ScatterRow/prepareScatterData.js
@@ -1,26 +1,61 @@
 import * as R from 'ramda';
 
-export function cleanRawData(rawDataArr, beaconLatlngsIndex) {
-  // const { beaconLatlngsIndex } = this.props;
-  const res = rawDataArr.slice(0, 100).map((el) => ({
-  // const res = rawDataArr.map((el) => ({
-    // const res = rawDataArr.filter(R.pipe(R.prop('loudestBeacon'), R.isNil, R.not)).map((el) => ({
-    ...el,
-    level: el.loudestBeacon ? el.loudestBeacon.level : -120,
-    // eslint-disable-next-line no-nested-ternary
-    placement: el.loudestBeacon
-      ? (beaconLatlngsIndex[el.loudestBeacon.beaconId] ? 'outdoors' : 'indoors')
-      : null,
-  }));
-  const res2 = R.groupBy((el) => (!el.loudestBeacon ? 'emptyMessages' : el.placement), res);
-  const filter = R.pipe(R.prop('loudestBeacon'), R.isNil, R.not);
-  const makeIndex = R.indexBy(R.path(['loudestBeacon', 'beaconId']));
-  const getBeaconIds = R.pipe(R.filter(filter), makeIndex, R.keys);
+// const hasBeacon = R.pipe(R.prop('loudestBeacon'), R.isNil, R.not);
+const hasBeacon = R.pipe(R.prop('beacon'), R.isNil, R.not);
 
-  const beaconIds = getBeaconIds(rawDataArr);
+export function cleanRawData(rawDataArr, beaconLatlngsIndex) {
+  const subArr = rawDataArr.slice(0, 100);
+  // const subArr = rawDataArr.slice(0, 1000);
+  // const subArr = rawDataArr;
+  // const subArr = rawDataArr.filter(hasBeacon);
+  // const res = R.unnest(subArr.map(messageToLoudestOrEmpty(beaconLatlngsIndex)));
+  const res = R.unnest(subArr.map(messageToAllBeaconsOrEmpty(beaconLatlngsIndex)));
+  const res2 = R.groupBy(R.prop('placement'), res);
+
+  // const makeIndex = R.indexBy(R.path(['loudestBeacon', 'beaconId']));
+  const makeIndex = R.indexBy(R.path(['beacon', 'beaconId']));
+  const getBeaconIds = R.pipe(R.filter(hasBeacon), makeIndex, R.keys);
+
+  const beaconIds = getBeaconIds(res);
 
   return {
     res: res2,
     beaconIds,
+  };
+}
+
+function messageToLoudestOrEmpty(beaconLatlngsIndex) {
+  return (el) => {
+    if (el.loudestBeacon) {
+      return [messageToBeacon(beaconLatlngsIndex, el, el.loudestBeacon)];
+    }
+    return [messageToEmpty(el)];
+  };
+}
+
+function messageToAllBeaconsOrEmpty(beaconLatlngsIndex) {
+  return (el) => {
+    if (el.beacons.length > 0) {
+      return el.beacons.map((beacon) => messageToBeacon(beaconLatlngsIndex, el, beacon));
+    }
+    return [messageToEmpty(el)];
+  };
+}
+
+function messageToEmpty(el) {
+  return {
+    ...el,
+    level: -120,
+    beacon: null,
+    placement: 'emptyMessages',
+  };
+}
+
+function messageToBeacon(beaconLatlngsIndex, el, beacon) {
+  return {
+    ...el,
+    level: beacon.level,
+    beacon,
+    placement: beaconLatlngsIndex[beacon.beaconId] ? 'outdoors' : 'indoors',
   };
 }
