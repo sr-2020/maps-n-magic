40d69b8f6f2979819c72b0a3e9ca6897dd70240e
NtsDK <ghoul71@yandex.ru>
2020-09-17 01:35:17 +0200

    Some cleaning.

--- packages/sr2020-mm-client/src/dataHOCs/withLocationRecords.jsx
+++ packages/sr2020-mm-client/src/dataHOCs/withLocationRecords.jsx
@@ -17,76 +17,8 @@ export const withLocationRecords = (Wrapped) => (props) => {
 
     return () => {
       gameModel.off('locationRecordsChanged2', updateLocations);
-      // group.clearLayers();
     };
   }, [gameModel]);
 
   return <Wrapped {...props} locationRecords={locationRecords} />;
 };
-
-// import React, { Component } from 'react';
-// import './ManaOceanLayer2.css';
-// import { InnerManaOceanLayer2 } from './InnerManaOceanLayer2';
-
-// export class ManaOceanLayer2 extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       locationRecords: [],
-//     };
-//     this.updateLocations = this.updateLocations.bind(this);
-//   }
-
-//   componentDidMount() {
-//     const {
-//       gameModel,
-//     } = this.props;
-//     this.subscribe('on', gameModel);
-//     this.updateLocations();
-//     console.log('ManaOceanLayer mounted');
-//   }
-
-//   componentDidUpdate(prevProps) {
-//     const {
-//       gameModel,
-//     } = this.props;
-//     if (prevProps.gameModel !== gameModel) {
-//       this.subscribe('off', prevProps.gameModel);
-//       this.subscribe('on', gameModel);
-//       this.updateLocations();
-//     }
-//     console.log('ManaOceanLayer did update');
-//   }
-
-//   componentWillUnmount() {
-//     const {
-//       gameModel,
-//     } = this.props;
-//     this.subscribe('off', gameModel);
-//     console.log('ManaOceanLayer will unmount');
-//   }
-
-//   updateLocations() {
-//     const {
-//       gameModel,
-//     } = this.props;
-//     this.setState({
-//       locationRecords: gameModel.get('locationRecords'),
-//     });
-//   }
-
-//   subscribe(action, gameModel) {
-//     gameModel[action]('locationRecordsChanged2', this.updateLocations);
-//   }
-
-//   render() {
-//     const { locationRecords } = this.state;
-//     // eslint-disable-next-line react/jsx-props-no-spreading
-//     return (
-//       <InnerManaOceanLayer2
-//         {...this.props}
-//         locationRecords={locationRecords}
-//       />
-//     );
-//   }
-// }
