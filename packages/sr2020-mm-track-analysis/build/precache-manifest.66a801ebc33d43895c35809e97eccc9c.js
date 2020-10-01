30664e31ebfc1eaa3bca5cd3d8c47e329eafa5f4
NtsDK <ghoul71@yandex.ru>
2020-10-01 04:40:24 +0200

    For now track analysis is not in development so it is prebuilt to save resources.

---
+++ packages/sr2020-mm-track-analysis/build/precache-manifest.66a801ebc33d43895c35809e97eccc9c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f726f4ac86af130460a84036c5cf70d0",
+    "url": "/trackAnalysis/index.html"
+  },
+  {
+    "revision": "62e5bf01f99ed115ec3c",
+    "url": "/trackAnalysis/static/css/2.fe152566.chunk.css"
+  },
+  {
+    "revision": "2f65f15ae4699a6282e2",
+    "url": "/trackAnalysis/static/css/main.39020a63.chunk.css"
+  },
+  {
+    "revision": "62e5bf01f99ed115ec3c",
+    "url": "/trackAnalysis/static/js/2.21a96dc1.chunk.js"
+  },
+  {
+    "revision": "fc433957ea39c3db2fd1a076c3b3b901",
+    "url": "/trackAnalysis/static/js/2.21a96dc1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f65f15ae4699a6282e2",
+    "url": "/trackAnalysis/static/js/main.0ab0aa74.chunk.js"
+  },
+  {
+    "revision": "f1adc4b43a63dd2042d8",
+    "url": "/trackAnalysis/static/js/runtime-main.e820ef0a.js"
+  }
+]);
