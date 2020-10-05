105b84d5123de33eee08a39fa55ccec26bcb5cc7
NtsDK <ghoul71@yandex.ru>
2020-10-06 01:46:24 +0200

    Try to improve audio auto play reaction

--- packages/sr2020-mm-client/src/components/RescueServiceSoundAlarm/RescueServiceSoundAlarm.jsx
+++ packages/sr2020-mm-client/src/components/RescueServiceSoundAlarm/RescueServiceSoundAlarm.jsx
@@ -2,22 +2,43 @@ import React, { useRef, useEffect, useState } from 'react';
 import './RescueServiceSoundAlarm.css';
 import * as R from 'ramda';
 
-let audio = null;
+// const audio = null;
+let audioPromise = null;
+let audioPromiseResolved = false;
 
-function playSound() {
-  if (!audio) {
-    // audio = new Audio('http://localhost:3000/sounds/274682__jppi-stu__sw-school-pa-alert.wav');
-    audio = new Audio('/sounds/274682__jppi-stu__sw-school-pa-alert.wav');
+function playSound(gameModel) {
+  if (!audioPromise) {
+    audioPromise = new Promise((resolve, reject) => {
+      // audio = new Audio('http://localhost:3000/sounds/274682__jppi-stu__sw-school-pa-alert.wav');
+      const audio = new Audio('/sounds/274682__jppi-stu__sw-school-pa-alert.wav');
+      audio.addEventListener('loadeddata', () => {
+        if (audio.readyState >= 2) {
+          audioPromiseResolved = true;
+          resolve(audio);
+          // obj.play();
+        }
+      });
+    });
   }
-  audio.play();
+  if (audioPromiseResolved) {
+    audioPromise.then((audio) => audio.play()).catch((err) => {
+      console.error('Some conditions are not meets', err);
+      gameModel.execute({
+        type: 'postNotification',
+        title: 'Запрещено воспроизведение звука',
+        message: 'Действие 1. Попробуйте закрыть это сообщение (серьезно, это не шутка).',
+        kind: 'error',
+      });
+    });
+  }
 }
 
 export function RescueServiceSoundAlarm(props) {
-  const { characterIdHealthList } = props;
+  const { characterIdHealthList, gameModel } = props;
   // const [list, setList] = useState([]);
   const prevList = usePrevious(characterIdHealthList);
   if (R.difference(characterIdHealthList, prevList).length > 0) {
-    playSound();
+    playSound(gameModel);
   }
   // console.log(characterIdHealthList);
   //
