93caa0bc7d0188078f6cb1c7725ece3dab5dfa16
NtsDK <ghoul71@yandex.ru>
2020-12-20 15:49:23 +0100

    Moved client API to other place

--- packages/sr2020-mm-event-engine/api/settings.js
+++ packages/sr2020-mm-event-engine/api/settings.js
@@ -1,3 +1,19 @@
+export const AUDIO_RETRANSLATOR = {
+  // SOUND_URL: 'http://localhost:3001',
+  SOUND_URL: 'https://maps-n-magic.evarun.ru',
+  // SOUND_URL: '',
+  SOUND_LIST_ROUTE: '/fileList',
+  SOUND_ROUTE: '/file',
+};
+
+export const SERVER_URL = 'http://localhost:3001';
+
+export const POST_USER_POSITION = '/postUserPosition';
+
+export function joinUrl(...args) {
+  return args.join('');
+}
+
 let WS_URL;
 if (process.env.NODE_ENV === 'production') {
   WS_URL = 'wss://maps-n-magic.evarun.ru/ws';
