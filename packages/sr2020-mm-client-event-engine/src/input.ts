908fa077884d0bbc05bac7f7047fed8d6e48c3cd
NtsDK <ghoul71@yandex.ru>
2021-03-02 04:27:17 +0100

    Add main file to client event engine

---
+++ packages/sr2020-mm-client-event-engine/src/input.ts
@@ -0,0 +1,8 @@
+export {
+  makeGameModel,
+} from './configs/clientEventEngine';
+
+// TODO this call should be moved in event engine service
+export { postUserPosition } from './api/position';
+
+export { getSound, getSoundList } from './api/sounds';
