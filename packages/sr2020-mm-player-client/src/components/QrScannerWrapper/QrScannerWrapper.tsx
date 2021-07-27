3ee1e8ab81824e4727756975fc32e6bffd2ef0f2
NtsDK <ghoul71@yandex.ru>
2021-07-27 23:54:10 +0500

    Experiment with torch light

--- packages/sr2020-mm-player-client/src/components/QrScannerWrapper/QrScannerWrapper.tsx
+++ packages/sr2020-mm-player-client/src/components/QrScannerWrapper/QrScannerWrapper.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import './QrScannerWrapper.css';
 
 import QRScanner, { QRCode } from 'qrx-scanner';
@@ -6,7 +6,7 @@ import { ErrorBoundary } from "react-error-boundary";
 import Button from "react-bootstrap/Button";
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSync } from '@fortawesome/free-solid-svg-icons';
+import { faSync, faLightbulb } from '@fortawesome/free-solid-svg-icons';
 // import { WithTranslation } from "react-i18next";
 
 interface QrScannerWrapperProps {
@@ -15,6 +15,8 @@ interface QrScannerWrapperProps {
   onCancel?: () => void;
   message?: string;
 }
+
+// type t = MediaStreamConstraints;
 
 let defaultFacingMode = process.env.NODE_ENV === 'production' 
   ? 'environment' 
@@ -30,12 +32,50 @@ export function QrScannerWrapper(props: QrScannerWrapperProps) {
   const { onFailed, onSuccess, message, onCancel } = props;
 
   const [facingMode, setFacingMode] = useState<string>(defaultFacingMode);
+  const [torch, setTorch] = useState<boolean>(false);
+
+  const qrScanner = useRef<QRScanner>(null);
   
   const videoConstraints: MediaTrackConstraints  = {
     facingMode: facingMode,
     height: 1080,
   };
   const defaultOnFailed = (error: Error) => {throw error};
+
+  useEffect(() => {
+    // based on 
+    // https://stackoverflow.com/questions/37848494/is-it-possible-to-control-the-camera-light-on-a-phone-via-a-website/47153547#47153547
+    const stream = qrScanner.current?.webcam?.stream;
+
+    if (stream) {
+      const track = stream.getVideoTracks()[0];
+
+      // const imageCapture = new ImageCapture(track);
+      // const photoCapabilities = await 
+      
+      // imageCapture.getPhotoCapabilities().then((photoCapabilities) => {
+      //   console.log('photoCapabilities', photoCapabilities);
+      // });
+
+      const data = track.getCapabilities();
+      // console.log('data', data);
+      
+      // track.applyConstraints({
+      //   // advanced: [{torch, brightness: torch ? 1 : 1}]
+      //   // advanced: [{torch}]
+      //   advanced: [{brightness: torch ? 100 : 100}]
+      // });
+      if (data.torch) {
+        
+        track.applyConstraints({
+          // advanced: [{torch, brightness: torch ? 0 : 1}]
+          advanced: [{torch}]
+          // advanced: [{brightness: torch ? 0 : 255}]
+        });
+      }
+
+    }
+  }, [qrScanner, torch]);
 
   return (
     <div className="QrScannerWrapper tw-flex tw-flex-col">
@@ -49,6 +89,7 @@ export function QrScannerWrapper(props: QrScannerWrapperProps) {
       }
       <ErrorBoundary fallback={<>Ошибка при сканировании QR</>}>
         <QRScanner
+          ref={qrScanner}
           // cursorResolution={600}
           minCursorSize={0.7}
           timeout={10 * 60000} // 10 minutes
@@ -82,6 +123,17 @@ export function QrScannerWrapper(props: QrScannerWrapperProps) {
           icon={faSync}
         />
       </Button> */}
+      <Button 
+        className="tw-absolute tw-right-0 tw-bg-green-900 tw-border-green-900 tw-opacity-70"
+        style={{top: '6rem'}}
+        onClick={() => setTorch(!torch)}
+        title="Вкл./Выкл. фонарик"
+      >
+        <FontAwesomeIcon
+          // icon={torch ? faLightbulb : faSync}
+          icon={faLightbulb}
+        />
+      </Button>
     </div>
   );
 }
